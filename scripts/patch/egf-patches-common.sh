94971acedc9ff031be98a5347e1e252d18747f0d
yocto <yocto@androidbuilder>
2023-05-05 10:48:34 +0200

    Eliminate cartelle duplicate da scripts/

--- scripts/patch/egf-patches-common.sh
+++
@@ -1,9 +0,0 @@
-
-# This map lists patch source and destination : "patch_src patch_dest" 
-# - patch_src -> path of the patch starting from ./meta-egf/scripts/egf-conf/egf-common
-# - patch_dest-> path of the destination from  source (../meta-egf)
-
-PATCH_MAP_COMMON=( 
-)
-
-
